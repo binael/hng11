c0ce4f1453d025cbbde50c7b803819d69d9cbe62
binael <nbinael@yahoo.com>
2023-09-07 22:13:12 +0100

    stage one project

---
+++ stage_one/dict_generator.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+A module that generates a dictionary for api data
+"""
+
+from datetime import datetime
+
+def dict_gen(slack_name: str, track: str) -> dict:
+    """
+    A function that generates a dictionary for api end
+    point
+
+    Parameters
+    ----------
+    slack_name: str
+        The slack username
+    track: str
+        The currect track in slack
+        Values may be 'backend, frondend' etc
+
+    Returns
+    -------
+    dict:
+        A dictionary to be that will be jsonified and returned
+    """
+
+    utc_datetime = datetime.utcnow()
+    date = utc_datetime.strftime("%y-%m-%dT%H:%M:%SZ")
+    weekday = utc_datetime.strftime("%A")
+
+    new_dict = {
+        "slack_name": slack_name,
+        "current_day": weekday,
+        "utc_time": date,
+        "track": track,
+        "github_file_url":,
+        "github_repo_url":,
+        "status_code": 200
+	}
